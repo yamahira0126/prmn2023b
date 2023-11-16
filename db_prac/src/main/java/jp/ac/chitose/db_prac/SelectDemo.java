3d2492b160a938f8627acb5fd63d1518635f4f30
yamahira0126 <b2222490@photon.chitose.ac.jp>
2023-11-16 12:25:01 +0900

    Deleteクラスを作成した

--- db_prac/src/main/java/jp/ac/chitose/db_prac/SelectDemo.java
+++ db_prac/src/main/java/jp/ac/chitose/db_prac/SelectDemo.java
@@ -18,7 +18,7 @@ public class SelectDemo {
                 preExam.print();
             }
 
-        } catch (SQLException e){
+        }catch (SQLException e){
             e.printStackTrace();
         }
     }
